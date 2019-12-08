802bf7583bc3514cd56e267658d4bb1653fbf01e
Sebastian Hans Weißenborn <SWT@Sebastians-MBP.fritz.box>
2019-12-08 02:37:20 +0100

    Updates

---
+++ precache-manifest.7a0d1b6b9ecdae14ec4e4ea05c47ed4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028000d391de421e33276aa30780b2de",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d8f1e578da131be523b3",
+    "url": "/monsters-rolodex/static/css/main.32c7b841.chunk.css"
+  },
+  {
+    "revision": "bae8cb79f8bb6fab8f2b",
+    "url": "/monsters-rolodex/static/js/2.9b5d238b.chunk.js"
+  },
+  {
+    "revision": "d8f1e578da131be523b3",
+    "url": "/monsters-rolodex/static/js/main.30e93823.chunk.js"
+  },
+  {
+    "revision": "63f2acb536a3e7c1eceb",
+    "url": "/monsters-rolodex/static/js/runtime-main.e8145f9b.js"
+  }
+]);
